5ab68e072eac9cf87354a759ffebb85ed7873efa
苏传宇 <AbelSu@suchuanyudemacbook-air.local>
2016-04-14 17:42:40 +0800

    Initial Commit

---
+++ DES/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DES
+//
+//  Created by 苏传宇 on 16/4/14.
+//  Copyright © 2016年 abelsu7. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
